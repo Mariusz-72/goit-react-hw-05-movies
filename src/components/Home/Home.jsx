a4a6afe3a11a5bae64bf9508e3c9d3610ce4a631
Mariusz-72 <mmbwdap@gmail.com>
2023-12-10 20:39:40 +0100

    update 2.4

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,57 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getTrendingMovies } from 'services/Api';
+
 import css from './Home.module.css';
 
 const Home = () => {
-    const [movies, setMovies] = useState([]);
+const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
+useEffect(() => {
         const fetchTrendingMovies = async () => {
-            try {
-                const response = await getTrendingMovies();
-                setMovies(response.results);
-            } catch (error) {
-                console.log('There is a problem with loading popular movies:', error);
-            }
-        };
+        try {
+            const response = await getTrendingMovies();
+            setMovies(response.results);
+        } catch (error) {
+            console.log(
+            'Wystąpił błąd podczas pobierania popularnych filmów:',
+            error
+            );
+        }
+};
 
         fetchTrendingMovies();
     }, []);
 
-    const [hoveredMovieId, setHoveredMovieId] = useState(null);
-    const handleMouseEnter = movieId => {
+const [hoveredMovieId, setHoveredMovieId] = useState(null);
+
+const handleMouseEnter = movieId => {
         setHoveredMovieId(movieId);
     };
-    const handleMouseLeave = () => {
+
+const handleMouseLeave = () => {
         setHoveredMovieId(null);
     };
 
-    return (
-        <div className={css.home}>
-            <h1>Popular movies</h1>
-            {movies.map(movie => (
-                <div
-                    key={movie.id}
-                    onMouseEnter={() => handleMouseEnter(movie.id)}
-                    onMouseLeave={handleMouseLeave}
-                    className={css.movieItem}
-                >
-                    <Link to={`/movies/${movie.id}`} className={css.movieLink}>
-                        <h3>{movie.title}</h3>
-                    </Link>
-                    {hoveredMovieId === movie.id && (
-                        <img
-                            src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
-                            alt={movie.title}
-                            className={css.moviePoster}
-                        />
-                    )}
-                </div>
-            ))}
-        </div>
-
-
+return (
+    <div className={css.home}>
+        <h1>Popularne filmy</h1>
+        {movies.map(movie => (
+            <div
+            key={movie.id}
+            onMouseEnter={() => handleMouseEnter(movie.id)}
+            onMouseLeave={handleMouseLeave}
+            className={css.movieItem}
+            >
+            <Link to={`/movies/${movie.id}`} className={css.movieLink}>
+                <h3>{movie.title}</h3>
+            </Link>
+            {hoveredMovieId === movie.id && (
+                <img
+                src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+                alt={movie.title}
+                className={css.moviePoster}
+                />
+            )}
+            </div>
+        ))}
+    </div>
     );
 };
 
--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -1,44 +1,44 @@
 import axios from 'axios';
-const API_KEY = '6909cd80103b2d22b83ee7f9c75d0eee';
+
+const API_KEY = '5eb4fde0c979574b7842bf5dec2210bc';
+
 const BASE_URL = 'https://api.themoviedb.org/3';
 
-
 const get = async (url, params = {}) => {
-    try {
-        const response = await axios.get(url, {
-            params: { ...params, api_key: API_KEY },
-        });
-        return response.data;
-    } catch (error) {
-        throw new Error(
-            error.response?.data?.status_message || 'Something went wrong'
-        );
-    }
+  try {
+    const response = await axios.get(url, {
+      params: { ...params, api_key: API_KEY },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      error.response?.data?.status_message || 'Something went wrong'
+    );
+  }
 };
 
-
 export const getTrendingMovies = () => {
-    const url = `${BASE_URL}/trending/movie/day`;
-    return get(url);
+  const url = `${BASE_URL}/trending/movie/day`;
+  return get(url);
 };
 
 export const searchMovies = query => {
-    const url = `${BASE_URL}/search/movie`;
-    const params = { query };
-    return get(url, params);
+  const url = `${BASE_URL}/search/movie`;
+  const params = { query };
+  return get(url, params);
 };
 
 export const getMovieDetails = movieId => {
-    const url = `${BASE_URL}/movies/${movieId}`;
-    return get(url);
+  const url = `${BASE_URL}/movie/${movieId}`;
+  return get(url);
 };
 
 export const getMovieCredits = movieId => {
-    const url = `${BASE_URL}/movie/${movieId}/credits`;
-    return get(url);
+  const url = `${BASE_URL}/movie/${movieId}/credits`;
+  return get(url);
 };
 
 export const getMovieReviews = movieId => {
-    const url = `${BASE_URL}/movie/${movieId}/reviews`
-    return get(url);
+  const url = `${BASE_URL}/movie/${movieId}/reviews`;
+  return get(url);
 };
